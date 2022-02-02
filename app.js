f0edfc62b725780ffad1d9103b41dcfed2261764
seanbreaksintotech <sbyrdie123@gmail.com>
2022-02-02 13:23:40 -0500

    app.js and index.html

---
+++ app.js
@@ -0,0 +1,7 @@
+const numberBttns = document.querySelectorAll('[data-number]')
+const operationBttns = document.querySelectorAll('[data-operation]')
+const equalsButtn = document.querySelector('[data-equals]')
+const deleteButtn = document.querySelector('[data-delete]')
+const allclearButtn = document.querySelector('[data-allclear]')
+const previousOperandTextElement = document.querySelector('[data-prevop]')
+const currentOperandTextElement = document.querySelector('[data-currentop]')
