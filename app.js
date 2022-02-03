3b59003e31eec3ee548513e76be322f85d9cbb85
seanbreaksintotech <sbyrdie123@gmail.com>
2022-02-03 11:08:37 -0500

    app.js and index.html

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement){
+    this.previousOperandTextElement = previousOperandTextElement
+    this.currentOperandTextElement = currentOperandTextElement
+    this.clear()
+}
+clear(){
+this.currentOperand = ''
+this.previousOperand = ''
+this.operation = undefined
+
+}
+delete() {
+
+}
+appendNumber (number){
+    this.currentOperand = number
+
+}
+chooseOperation (operation){
+
+}
+
+ compute(){
+
+ }   
+ 
+ updateDisplay(){
+    this.currentOperandTextElement.innerText = this.currentOperand
+ }
+}
+
 const numberBttns = document.querySelectorAll('[data-number]')
 const operationBttns = document.querySelectorAll('[data-operation]')
 const equalsButtn = document.querySelector('[data-equals]')
@@ -5,3 +37,15 @@ const deleteButtn = document.querySelector('[data-delete]')
 const allclearButtn = document.querySelector('[data-allclear]')
 const previousOperandTextElement = document.querySelector('[data-prevop]')
 const currentOperandTextElement = document.querySelector('[data-currentop]')
+
+
+const calculator = new Calculator (previousOperandTextElement, currentOperandTextElement)
+
+numberBttns.forEach(button => {
+    button.addEventListener('click', () =>{
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+ 
